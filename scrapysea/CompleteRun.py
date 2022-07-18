e2cb6188475576aa89cd1343c28e6e9c3053dd1c
edgar yong <edgar.yong.yip.han@gmail.com>
2022-07-19 02:37:44 +0800

    Pulling SF Data

--- scrapysea/CompleteRun.py
+++ scrapysea/CompleteRun.py
@@ -51,7 +51,7 @@ def exec_Crawler():
     #        continue
     #    runner.crawl(s)
 
-    runner.crawl(CalculationsData.PotentialSpider)
+    runner.crawl(CalculationsData.StarforceSpider)
     d = runner.join()
     d.addBoth(lambda _: reactor.stop())
     reactor.run()
