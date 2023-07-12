c4b0c4ce3f0f02b7373323e4363abe9e9cd765dc
edgar <eyong@EdgarPC>
2023-07-13 00:59:37 +0800

    Reorg File structure

--- scrapysea/spiders/DevSpider.py
+++ scrapysea/spiders/DevSpider.py
@@ -4,10 +4,13 @@ import scrapysea.utils as utils
 
 DLog = utils.Set_Custom_Logger("Dev Spider", logTo="./Logs/Dev.log", propagate=False)
 
-
 class DevSpider(scrapy.Spider):
     name = "DevSpider"
     ID = "Potential"
     start_urls = [utils.Source_Links[ID]]
 
+    def parse(self, response):
+
+
+        ...
     ...
